b8bcf1cdc7daed475974ab5c082a5c4dfeb9940d
mariando <mariando@mariandonoMacBook-Air.local>
2025-07-05 07:47:54 +0900

    fix:RuboCop offenses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
   def ogp_title
     content_for?(:ogp_title) ? content_for(:ogp_title) : "キックボクシングスタイル診断アプリ"
   end
-  
+
   def ogp_description
     content_for?(:ogp_description) ? content_for(:ogp_description) : "あなたに合ったキックボクシングのスタイルがわかる！"
   end
-  
+
   def ogp_image
     image_url(content_for?(:ogp_image) ? content_for(:ogp_image) : "ogp.png")
   end
