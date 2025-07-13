537793f8e240b6123b1cd1515dbe14b19c33dce2
mariando <mariando@mariandonoMacBook-Air.local>
2025-07-13 14:30:43 +0900

    add スタイル一覧

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,5 +1,9 @@
 class ResultsController < ApplicationController
   def show
     @result = Result.find_by(category: params[:id])
+    session[:last_result_category] = @result.category if @result.present?
+  end
+  def index
+    @results = Result.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
 
   get "questions/reset", to: "questions#reset", as: "reset_questions"
 
+  resources :results, only: [ :index ]
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
